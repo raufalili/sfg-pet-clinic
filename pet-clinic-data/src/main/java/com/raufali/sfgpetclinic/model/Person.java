c4d7912811e8dc17f041539c95db85b38a75b2d1
raufalili <raufelili@gmail.com>
2021-05-22 13:03:59 -0700

    Adding base entity. closes #10

--- pet-clinic-data/src/main/java/com/raufali/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/raufali/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.raufali.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/raufali/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/raufali/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.raufali.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
