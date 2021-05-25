78731bfcf488fef896d4729eddf33a51dcbbd347
raufalili <raufelili@gmail.com>
2021-05-25 12:41:55 -0700

    Added Pets and contact info to owners on startup. closes #32

--- pet-clinic-web/src/main/java/com/raufali/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/raufali/sfgpetclinic/bootstrap/DataLoader.java
@@ -10,6 +10,8 @@ import com.raufali.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
@@ -37,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickell");
+        owner1.setCity("Miami");
+        owner1.setTelephone("65511316511");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickell");
+        owner2.setCity("Miami");
+        owner2.setTelephone("65511316511");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Mesi");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
